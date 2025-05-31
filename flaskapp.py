7940e8c2935659ca7db53b3be22e12a48bb69f6e
7arhan1 <L00187444@atu.ie>
2025-05-31 21:47:24 +0000

    change in falskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -23,10 +23,9 @@ def predict():
     data = pd.DataFrame([[year]], columns=["Year"])
 
     # Uncomment this for actual prediction:
-    #prediction = model.predict(data)
-    prediction = 333.345
+    prediction = model.predict(data)
     formatted_prediction = f"Predicted EV stock share for {int(year)}: {round(float(prediction[0]), 2)}%"
-    return render_template("result.html", prediction=formatted_prediction)
+    return render_template("result.html", prediction=456.00)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
